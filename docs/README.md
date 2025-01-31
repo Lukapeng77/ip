9ce7dd2e91e317ea0dea332b244b794ef80c3d0f
lukapeng77 <e1249364@u.nus.edu>
2025-01-31 14:37:53 +0800

    Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Luka77 User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Luka77.java
@@ -0,0 +1,13 @@
+public class Luka77 {
+    public static void main(String[] args) {
+        String chatbot_name = "Luka77";
+        String output = "____________________________________________________________\n"
+                + " Hello! I'm " + chatbot_name + "\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n";
+        System.out.println(output);
+        
+        System.out.println("Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n");
+    }
+}
